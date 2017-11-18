5abd94791dfc9125ba5b5891766d9eaa0a73e180
abetusk <abetusk@gmail.com>
2017-11-18 16:50:32 -0500

    ...

--- projects/laser-relief/aesop/creat-gcode.sh
+++ projects/laser-relief/aesop/creat-gcode.sh
@@ -18,11 +18,16 @@ S_engrave="S0.25"
 S_move="S0.0"
 
 F_cut="F800"
-F_engrave="F1000"
-F_move="F3000"
+F_engrave="F4000"
+F_move="F8000"
 
 xshift=30
 yshift=30
+
+#rx=238
+rx=20
+ry=20
+
 
 if [[ "$VERBOSE" -eq 1 ]] ; then
   echo "##"
@@ -44,7 +49,7 @@ mkdir -p ngc
 
 for pfx_fn in $pfx_fns ; do
   b=`basename $pfx_fn .png`
-  fn="${pfx_fn}-mask_solid.png"
+  fn="${pfx_fn}-mask.png"
 
   echo "$b $fn ..."
 
@@ -54,6 +59,9 @@ for pfx_fn in $pfx_fns ; do
   echo -e "G21\nG90\nG1 $F_cut S0\nG0 $F_move S0\n" > ngc/$b-outline.ngc
   gp2ngc -i aux/$b-ord.gp --sfx-rapid "$F_rapid $S_move" --sfx-slow "$F_cut $S_cut" | \
     ngc_scale -s "$finmul" >> ngc/$b-outline.ngc
+  ngc_rotate -f ngc/$b-outline.ngc -d 90 | \
+    ngc_scale -x "-1" -y "1" | \
+    ngc_translate -x $rx -y $ry -U -S > ngc/$b-outline-rot90.ngc
     #ngc_scale -s "$invmul" > ngc/$b.ngc
 done
 
@@ -61,11 +69,42 @@ for pfx_fn in $pfx_fns ; do
   b=`basename $pfx_fn .png`
   ngc_fn="${pfx_fn}-relief.gcode"
 
+  dos2unix "$ngc_fn"
+  sed -i 's/;.*//g' "$ngc_fn"
+
   cat $ngc_fn > ngc/$b.ngc
   cat ngc/$b-outline.ngc >> ngc/$b.ngc
   cat ngc/$b-outline.ngc >> ngc/$b.ngc
 done
 
+dos2unix layers/layer3-relief.gcode
+sed -i 's/;.*//g' layers/layer3-relief.gcode
+
 cat layers/layer3-relief.gcode > ngc/layer3.ngc
 cat layers/box.ngc >> ngc/layer3.ngc
 cat layers/box.ngc >> ngc/layer3.ngc
+
+for z in {0..3} ; do
+
+  ngc_rotate -f ngc/layer$z.ngc -d 90 | \
+    ngc_scale -x "-1" -y "1" | \
+    ngc_translate -x $rx -y $ry -U -S | \
+    sed 's/S/ S/g' | sed 's/F/ F/' | sed 's/^ *//' | \
+    sed 's/G1 *'$F_engrave'/G1 '$F_engrave'\nG0 '$F_move'/' > ngc/layer$z-rot90.ngc
+
+done
+
+#exit
+#
+#ngc_rotate -f ngc/layer1.ngc -d 90 | \
+#  ngc_translate -x $rx -y $ry -U -S | \
+#  sed 's/S/ S/g' | sed 's/F/ F/' | sed 's/^ *//' > ngc/layer1-rot90.ngc
+#
+#ngc_rotate -f ngc/layer2.ngc -d 90 | \
+#  ngc_translate -x $rx -y $ry -U -S | \
+#  sed 's/S/ S/g' | sed 's/F/ F/' | sed 's/^ *//' > ngc/layer2-rot90.ngc
+#
+#ngc_rotate -f ngc/layer3.ngc -d 90 | \
+#  ngc_translate -x $rx -y $ry -U -S | \
+#  sed 's/S/ S/g' | sed 's/F/ F/' | sed 's/^ *//' > ngc/layer3-rot90.ngc
+
