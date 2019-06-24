3b89ba4c905887517abc8daeee269820e3a2f39f
abetusk <abetusk@mechaelephant.com>
2019-06-23 20:35:27 -0400

    moved jgarff/rpi_ws281x to local dir

--- projects/inner-light/src/inner-light-drive/cmp.sh
+++ projects/inner-light/src/inner-light-drive/cmp.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-incdir="/home/pi/git/github/jgarff/rpi_ws281x"
-cfn0="/home/pi/git/github/jgarff/rpi_ws281x/ws2811.c"
-cfn1="/home/pi/git/github/jgarff/rpi_ws281x/pwm.c"
-cfn2="/home/pi/git/github/jgarff/rpi_ws281x/pcm.c"
-cfn3="/home/pi/git/github/jgarff/rpi_ws281x/rpihw.c"
-cfn4="/home/pi/git/github/jgarff/rpi_ws281x/mailbox.c"
-cfn5="/home/pi/git/github/jgarff/rpi_ws281x/dma.c"
+#incdir="/home/pi/git/github/jgarff/rpi_ws281x"
+#cfn0="/home/pi/git/github/jgarff/rpi_ws281x/ws2811.c"
+#cfn1="/home/pi/git/github/jgarff/rpi_ws281x/pwm.c"
+#cfn2="/home/pi/git/github/jgarff/rpi_ws281x/pcm.c"
+#cfn3="/home/pi/git/github/jgarff/rpi_ws281x/rpihw.c"
+#cfn4="/home/pi/git/github/jgarff/rpi_ws281x/mailbox.c"
+#cfn5="/home/pi/git/github/jgarff/rpi_ws281x/dma.c"
+
+incdir="rpi_ws281x"
+cfn0="rpi_ws281x/ws2811.c"
+cfn1="rpi_ws281x/pwm.c"
+cfn2="rpi_ws281x/pcm.c"
+cfn3="rpi_ws281x/rpihw.c"
+cfn4="rpi_ws281x/mailbox.c"
+cfn5="rpi_ws281x/dma.c"
 
 gcc inner-light-drive.c $cfn0 $cfn1 $cfn2 $cfn3 $cfn4 $cfn5 -o inner-light-drive -I $incdir
 g++ inner-light-map.c -o inner-light-map
---
+++ projects/inner-light/src/inner-light-drive/rpi_ws281x/version.h
@@ -0,0 +1,10 @@
+/* Auto Generated Header built by version.py - DO NOT MODIFY */
+
+#ifndef __VERSION_H__
+#define __VERSION_H__
+
+#define VERSION_MAJOR 1
+#define VERSION_MINOR 1
+#define VERSION_MICRO 0
+
+#endif /* __VERSION_H__ */
