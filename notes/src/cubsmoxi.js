5eca86d99ae85417e45ca0fc69ceaa9f5ca2245d
abetusk <abetusk@gmail.com>
2019-08-31 11:41:53 -0400

    ex 1 4

--- notes/src/cubsmoxi.js
+++ notes/src/cubsmoxi.js
@@ -23,13 +23,7 @@ var example_melody1 = {
   "tune_only": true
 };
 
-function _irand(n, m) {
-  if (typeof(m) === "undefined") {
-    n = ((typeof(n) === "undefined") ? 1 : n);
-    return Math.floor(Math.random()*n);
-  }
-  return Math.floor(Math.random()*(m-n)) + n;
-}
+//----
 
 function newMelody() {
   var m = {};
@@ -39,13 +33,145 @@ function newMelody() {
   return m;
 }
 
+
+//----
+
+function normalize_note(note) {
+  note = ((typeof note === "undefined") ? "  ": note);
+  if (note == "") { return "  "; }
+  if (note == " ") { return "  "; }
+  if (note.length == 1) { return note + "4"; }
+  return note.substr(0,2);
+}
+
+//----
+
+function discritize_tune(tune, beats_per_measure, resolution) {
+  beats_per_measure = ((typeof beats_per_measure === "undefined") ? 4 : beats_per_measure);
+  resolution = ((typeof resolution === "undefined") ? 32 : resolution);
+
+  var da = [];
+  var note = "";
+
+  for (var i=0; i<tune.length; i++) {
+    note = normalize_note(tune[i].note);
+
+    var m = resolution*tune[i].dur
+    for (var j=0; j<m; j++) {
+      if (j==0) {
+        da.push(note + "s");
+      }
+      else if (j==(m-1)) {
+        da.push(note + ";");
+      }
+      else {
+        da.push(note + ".");
+      }
+    }
+  }
+
+  return da;
+}
+
+//----
+
+function _irand(n, m) {
+  if (typeof(m) === "undefined") {
+    n = ((typeof(n) === "undefined") ? 1 : n);
+    return Math.floor(Math.random()*n);
+  }
+  return Math.floor(Math.random()*(m-n)) + n;
+}
+
+//----
+
+function check_rhythm(M, rhythms, beats_per_measure, resolution) {
+  resolution = ((typeof resolution === "undefined") ? 1 : resolution);
+  beats_per_measure = ((typeof beats_per_measure === "undefined") ? 4 : beats_per_measure);
+
+  var stride = beats_per_measure * resolution;
+  var dtune = discritize_tune(M.tune, beats_per_measure, resolution);
+  var vrs = [];
+  
+  // beat token for simplicity of comparison
+  //
+  for (var ii=0; ii<rhythms.length; ii++) {
+    var cur = [];
+    for (var jj=0; jj<rhythms[ii].length; jj++) {
+      var n = Math.abs(rhythms[ii][jj]);
+
+      cur.push('s');
+      for (var x=1; x<(n-1); x++) {
+        cur.push('.');
+      }
+      if (n>1) { cur.push(';'); }
+    }
+    vrs.push(cur);
+  }
+
+  // check valid rhythms
+  //
+  for (var _s=0; _s<dtune.length; _s += stride) {
+    var found = 0;
+    for (var r_idx=0; r_idx < vrs.length; r_idx++) {
+      var ii=0;
+      for (ii=0; ii<stride; ii++) {
+        if (dtune[_s+ii][2] != vrs[r_idx][ii]) { break; }
+      }
+      if (ii==stride) { found=1; break; }
+    }
+    if (found==0) { return -1; }
+  }
+
+  return 0;
+}
+
+//----
+
+function dtune_measure_repetition_count(dtune, stride) {
+  var nmeasure = dtune.length / stride;
+
+  var map = {};
+
+  for (var ii=0; ii<nmeasure; ii++) {
+    var key = dtune.slice(ii*stride, ii*stride+stride).join("");
+    if (key in map) { map[key] += 1; }
+    else { map[key] = 0; }
+  }
+
+  var n_rep=0;
+  for (var key in map) {
+    n_rep += map[key];
+  }
+  return n_rep;
+
+  // check for repeated measure
+  //
+  var n_rep = 0;
+  for (var ii=0; ii<nmeasure; ii++) {
+    for (var jj=(ii+1); jj<nmeasure; jj++) {
+      var _str0 = dtune.slice(ii*stride, ii*stride+stride).join("");
+      var _str1 = dtune.slice(jj*stride, jj*stride+stride).join("");
+
+      if (_str0 != _str1) { continue; }
+      n_rep += 1;
+    }
+  }
+
+  return n_rep;
+}
+
+//----
+
 function melody_to_sonicpi(m) {
   var dt = 0.25;
   console.log("use_synth :dsaw");
   console.log("dt =", dt);
 
   for (var i=0; i<m.tune.length; i++) {
-    console.log("play :" + m.tune[i].note);
+    if (normalize_note(m.tune[i].note)!="  ") {
+      console.log("play :" + m.tune[i].note);
+    }
     console.log("sleep " + m.tune[i].dur*dt);
   }
 }
@@ -76,41 +202,6 @@ function general_rule(m) {
   //
 
 
-}
-
-function normalize_note(note) {
-  note = ((typeof note === "undefined") ? "  ": note);
-  if (note == "") { return "  "; }
-  if (note == " ") { return "  "; }
-  if (note.length == 1) { return note + "4"; }
-  return note.substr(0,2);
-}
-
-function discritize_tune(tune, beats_per_measure, resolution) {
-  beats_per_measure = ((typeof beats_per_measure === "undefined") ? 4 : beats_per_measure);
-  resolution = ((typeof resolution === "undefined") ? 32 : resolution);
-
-  var da = [];
-  var note = "";
-
-  for (var i=0; i<tune.length; i++) {
-    note = normalize_note(tune[i].note);
-
-    var m = resolution*tune[i].dur
-    for (var j=0; j<m; j++) {
-      if (j==0) {
-        da.push(note + "s");
-      }
-      else if (j==(m-1)) {
-        da.push(note + ";");
-      }
-      else {
-        da.push(note + ".");
-      }
-    }
-  }
-
-  return da;
 }
 
 
@@ -336,7 +427,7 @@ function ex_1_2_sample() {
   var _M = newMelody();
   _M.beats_per_measure = beats_per_measure;
 
-  // not map for easy lookup
+  // note map for easy lookup
   //
   for (var i=0; i<valid_notes.length; i++) {
     note_map[valid_notes[i]] = 1;
@@ -403,5 +494,222 @@ function ex_1_2_sample() {
 //-------------
 //-------------
 //-------------
+
+
+function ex_1_3(M) {
+  var valid_note_seq = ["d4", "a4", "f4", "e4", "c4"];
+  var beats_per_measure = 8;
+  var valid_rhythm = [[2,1,1,4],[-2,2,2,2]];
+  var vrs = [['s', ';', 's', 's', 's', '.', '.', ';'],['s',';','s',';','s',';','s',';']];
+
+  var nmeasure = [6,10];
+
+  var vrs_idx = [];
+  for (var i=0; i<valid_rhythm.length; i++) { vrs_idx.push(0); }
+
+  var stride = beats_per_measure * 1;
+  var dtune = discritize_tune(M.tune, beats_per_measure, 1);
+  var m = dtune.length;
+
+  // Check measure length in range
+  //
+  var _actual_measure = dtune.length / stride;
+  if ((_actual_measure < nmeasure[0]) ||
+      (_actual_measure > nmeasure[1])) {
+    return -1;
+  }
+
+  // check valid rhythms
+  //
+  for (var _s=0; _s<dtune.length; _s += stride) {
+    var found = 0;
+    for (var r_idx=0; r_idx < vrs.length; r_idx++) {
+      var ii=0;
+      for (ii=0; ii<stride; ii++) {
+        if (dtune[_s+ii][2] != vrs[r_idx][ii]) { break; }
+      }
+      if (ii==stride) { found=1; break; }
+    }
+    if (found==0) { return -1; }
+  }
+
+  // check valid note progression
+  //
+  var seq_idx = 0, seq_idx_prv = 0;
+  var seq_n = valid_note_seq.length;
+  for (var ii=0; ii < M.tune.length; ii++) {
+
+    var note = normalize_note(M.tune[ii].note);
+    if (note == valid_note_seq[seq_idx]) {
+      seq_idx = (seq_idx+1) % seq_n;
+      seq_idx_prv = (seq_idx + (seq_n-1)) % seq_n;
+    }
+    else if ((note == valid_note_seq[seq_idx_prv]) || (note == "  ")) {
+      // still ok
+    }
+    else {
+      return -1;
+    }
+
+  }
+
+  score = 0;
+
+  return score;
+
+}
+
+
+function ex_1_3_sample() {
+
+  var valid_note_seq = ["d4", "a4", "f4", "e4", "c4"];
+  var beats_per_measure = 8;
+  var valid_rhythm = [[2,1,1,4],[-2,2,2,2]];
+  var nmeasure = [6,10];
+
+  var m = _irand(nmeasure[0], nmeasure[1]+1);
+
+  var _M = newMelody();
+  _M.beats_per_measure = beats_per_measure;
+
+  var note_idx = 0;
+  for (var ii=0; ii<m; ii++) {
+
+    var r_idx = _irand(valid_rhythm.length);
+    for (var jj=0; jj<valid_rhythm[r_idx].length; jj++) {
+
+      var note = "  ";
+      if (valid_rhythm[r_idx][jj] > 0) {
+        note = valid_note_seq[note_idx];
+        var ndel = _irand(2);
+        note_idx = (note_idx + ndel) % valid_note_seq.length;
+      }
+
+      _M.tune.push({"note":note, "dur":Math.abs(valid_rhythm[r_idx][jj])});
+
+    }
+
+  }
+
+  var score = ex_1_3(_M);
+
+  console.log("# nmeasure:", m, ", score:", score);;
+  console.log(_M.tune);
+  melody_to_sonicpi(_M);
+
+}
+
+//ex_1_3_sample();
+
+//-------------
+//-------------
+//-------------
+
+
+function ex_1_4(M) {
+  var valid_notes = ["c4", "d4", "e4", "f4", "g4", "a4", "b4", "c5"];
+  var beats_per_measure = 8;
+  var valid_rhythm = [[2,1,1,4]];
+  var vrs = [['s',';','s','s','s','.','.',';']];
+  var measure_rep = [1,3];
+  var measure_range = [6,6];
+
+  var vrs_idx = [];
+  for (var i=0; i<valid_rhythm.length; i++) { vrs_idx.push(0); }
+
+  var stride = beats_per_measure * 1;
+  var dtune = discritize_tune(M.tune, beats_per_measure, 1);
+  var m = dtune.length;
+
+  // Check measure length in range
+  //
+  var _actual_measure = dtune.length / stride;
+  if ((_actual_measure < measure_range[0]) ||
+      (_actual_measure > measure_range[1])) {
+    return -1;
+  }
+
+  if (check_rhythm(M, valid_rhythm, beats_per_measure, 1)!=0) {
+    return -1;
+  }
+
+  var start_note = normalize_note(M.tune[0].note);
+  var end_note = normalize_note(M.tune[ M.tune.length-1 ].note);
+  if (start_note != end_note) {
+    return -1;
+  }
+
+  var ret = 0;
+  var n_rep = dtune_measure_repetition_count(dtune, stride);
+  if ((n_rep >= measure_rep[0]) &&
+      (n_rep <= measure_rep[1])) { ret += n_rep; }
+  else if (n_rep > measure_rep[1]) { ret += measure_rep[1]; }
+
+  return ret;
+}
+
+function ex_1_4_sample() {
+  var valid_notes = ["c4", "d4", "e4", "f4", "g4", "a4", "b4", "c5"];
+  var beats_per_measure = 8;
+  var valid_rhythm = [[2,1,1,4]];
+  var measure_rep = [1,3];
+  var measure_range = [6,6];
+
+  var nmeasure = _irand(measure_range[0], measure_range[1]+1);
+  var nrep = _irand(measure_rep[0], measure_rep[1]+1);
+
+  var meas = [];
+
+  var M = newMelody();
+  M.beats_per_measure = beats_per_measure;
+
+  // use pigeonhole to get repetitions
+  //
+  var nmeaslib = nmeasure - nrep;
+  var meas_lib = [];
+
+  for (var ii=0; ii<nmeaslib; ii++) {
+    var meas = [];
+    var r_idx = _irand(valid_rhythm.length);
+    for (var jj=0; jj<valid_rhythm[r_idx].length; jj++) {
+      var note = valid_notes[_irand(valid_notes.length)];
+      var dur = valid_rhythm[r_idx][jj];
+      meas.push({ "note":note, "dur":dur });
+    }
+
+    // force c4 as root of first measure, c4 as end of last measure.
+    // assume first measure is meas_lib[0] and last measure is
+    // meas_lib[ n-1 ].
+    //
+
+    if (ii==0) { meas[0].note = valid_notes[0]; }
+    if (ii==(nmeaslib-1)) { meas[ meas.length-1 ].note = valid_notes[0]; }
+
+    meas_lib.push(meas);
+  }
+
+  for (var ii=0; ii<nmeasure; ii++) {
+    var m_idx = _irand(meas_lib.length);
+    if (ii==0) { m_idx=0; }
+    if (ii==(nmeasure-1)) { m_idx = meas_lib.length-1; }
+
+    for (var jj=0; jj<meas_lib[m_idx].length; jj++) {
+      M.tune.push( meas_lib[m_idx][jj] );
+    }
+  }
+
+  var score = ex_1_4(M);
+  console.log("# score", score);
+  melody_to_sonicpi(M);
+
+
+}
+
+//ex_1_4_sample();
+
+//-------------
+//-------------
+//-------------
+
 
 
